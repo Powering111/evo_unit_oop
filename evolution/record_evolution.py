424366b38181389c70c92eb0570d1dc428c5db0e
coco483 <151052125+coco483@users.noreply.github.com>
2024-06-13 19:27:19 +0900

    modify format and bug fix

--- evolution/record_evolution.py
+++ evolution/record_evolution.py
@@ -1,12 +1,15 @@
 import csv
 import os
+from datetime import datetime
+from evolution.settings import *
 
 def write_to_csv(target, class_name, fitness_records):
-    fields = [f"testSuite{i}" for i in range(10)]
+    fields = [f"testSuite{i}" for i in range(1, POP_PER_GEN+1)]
     fields.insert(0, "generation")
     if not os.path.isdir(f"record/{target}"):
         os.makedirs(f"record/{target}")
-    with open(f"record/{target}/record_{class_name}.csv", 'w', newline='') as csvfile:
+    curr_time = datetime.now().strftime('%Y%m%d_%H%M%S')
+    with open(f"record/{target}/record_{class_name}_{curr_time}.csv", 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
 
         csvwriter.writerow(fields)
